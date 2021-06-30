5b6e666fb52d243efa061a29bddb6d47df2f376e
MalkonF <sudo777@protonmail.com>
2021-06-30 17:16:57 -0300

    Vendo vários tipos de collections e métodos em comuns entre elas

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,5 +1,37 @@
 package br.com.alura
 
 fun main() {
+//vc pode usar comportamentos comuns entre as Collections já que list, set, map estão relacionados
+    val nomes: List<String> = listOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
+    val nomes2: Collection<String> = listOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
+
+    val nomes3: Iterable<String> = setOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
+    for (nome in nomes)//aqui é um exemplo de um list utilizando iterator do Iterable
+        println(nome)
+
+    println(nomes)
+    println(nomes2)
+    println(nomes3)
+
+    println("Tem o nome Alex? ${nomes.contains("Alex")}")
+    println("Tamanho da coleção ${nomes.size}")//aqui nomes3 n vai funcionar, pois é uma Iterable e n compartilha o mesmo comportamento
 
 }
