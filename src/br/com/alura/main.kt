edf805631ff76fe880d04053d3b54531cbedec85
MalkonF <sudo777@protonmail.com>
2021-07-01 13:17:01 -0300

    Juntando listas com  +

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -4,8 +4,6 @@ fun main() {
     val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
     val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
 
-    val assistiramAmbos = mutableListOf<String>()
-    assistiramAmbos.addAll(assistiramCursoAndroid)
-    assistiramAmbos.addAll(assistiramCursoKotlin)
+    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
     println(assistiramAmbos)
 }
