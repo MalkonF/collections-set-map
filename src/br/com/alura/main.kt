79a8b6ca9a4646052fc1ea25be11ed048b5b3449
MalkonF <sudo777@protonmail.com>
2021-07-02 19:17:49 -0300

    Utilizando o Map

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,29 +1,22 @@
 package br.com.alura
 
 fun main() {
-    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin: Set<String> = setOf("Alex", "Paulo", "Maria")
+    val pedidos = mapOf<Int, Double>(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0 // infix tem o mesmo valor que o Pair() só perde na performance
+    )
+    println(pedidos)
+    val pedido: Double? = pedidos[1]
+    pedido?.let {
+        println("Pedido $it") //pega o pedid[1]
+    }
 
-    val assistiramAmbos = mutableSetOf<String>()
+    for (pedido in pedidos) {
+        println("Numero do pedido ${pedido.key}")
+        println("Valor do pedido ${pedido.value}")
 
-    assistiramAmbos.addAll(assistiramCursoAndroid)
-    assistiramAmbos.addAll(assistiramCursoKotlin)
-    assistiramAmbos.add("Ana")
-    assistiramAmbos.add("Ana")
-    println(assistiramAmbos)
-
-    // faz a mesma coisa linha 16 e 17
-    println(assistiramCursoAndroid + assistiramCursoKotlin)
-    println(assistiramCursoAndroid.union(assistiramCursoKotlin))
-    println(assistiramCursoAndroid union assistiramCursoKotlin) //in fix
-
-    println(assistiramCursoAndroid - assistiramCursoKotlin)
-    println(assistiramCursoAndroid subtract assistiramCursoKotlin)
-    //pega as pessoas que assistiram ambos os cursos
-    println(assistiramCursoAndroid intersect assistiramCursoKotlin)
-
-    val toMutableList = assistiramAmbos.toMutableList()
-    toMutableList.add("Alex")
-    println(toMutableList) //volta a repetir os elementos pois foi transformada para uma lista mutável
-    println(toMutableList.toSet())//volta a retirar os elementos repetidos
+    }
 }
+
+
---
+++ src/br/com/alura/testaSet.kt
@@ -0,0 +1,29 @@
+package br.com.alura
+
+fun testaSet() {
+    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: Set<String> = setOf("Alex", "Paulo", "Maria")
+
+    val assistiramAmbos = mutableSetOf<String>()
+
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
+
+    // faz a mesma coisa linha 16 e 17
+    println(assistiramCursoAndroid + assistiramCursoKotlin)
+    println(assistiramCursoAndroid.union(assistiramCursoKotlin))
+    println(assistiramCursoAndroid union assistiramCursoKotlin) //in fix
+
+    println(assistiramCursoAndroid - assistiramCursoKotlin)
+    println(assistiramCursoAndroid subtract assistiramCursoKotlin)
+    //pega as pessoas que assistiram ambos os cursos
+    println(assistiramCursoAndroid intersect assistiramCursoKotlin)
+
+    val toMutableList = assistiramAmbos.toMutableList()
+    toMutableList.add("Alex")
+    println(toMutableList) //volta a repetir os elementos pois foi transformada para uma lista mutável
+    println(toMutableList.toSet())//volta a retirar os elementos repetidos
+}
