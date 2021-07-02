b45c392b65a32b814c718f62c377f5bf4d6de739
MalkonF <sudo777@protonmail.com>
2021-07-01 22:39:51 -0300

    Utilizando Set

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,9 +1,15 @@
 package br.com.alura
 
 fun main() {
-    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
+    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: List<String> = listOf("Alex", "Paulo", "Maria")
 
-    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
-    println(assistiramAmbos.distinct())
+    //val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid //se inverter dá erro na linha 9. Pq
+    // ele retorna o tipo do primeiro arg ou seja o assistiramCursoAndroid que é um set e n da pra fazer
+    //[0] em sets
+    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+
+    println(assistiramAmbos)
+
+//println(assistiramAmbos[0])// pegando primeiro elemento da lista
 }
